1f038bd9d2dc11101127851f915b13bdcf0628d3
detodavide <davide.marchetti931@gmail.com>
2023-12-29 15:08:44 +0000

    get ip

--- db_connection.py
+++ db_connection.py
@@ -14,8 +14,19 @@ def connect_to_db(database):
     except Exception as e:
         print(f"An error occurred while connecting to the database: {str(e)}")
         return None
+    
+import socket
+import streamlit as st
+
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
 
 if __name__ == '__main__':
+
+    ip = get_ip_address()
+    st.write(f"The current IP address of the Streamlit app is: {ip}")
     db = connect_to_db("DiscoData")
     if db is not None:
         bigdata1 = db.botDiscoLogs
